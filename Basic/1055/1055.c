60a73ed3e983ac25427c77be7ab15e1a489bb2de
jetou <1021644861@qq.com>
2016-06-06 13:51:00 +0800

    Basic Level

--- Basic/1055/1055.c
+++ Basic/1055/1055.c
@@ -3,10 +3,11 @@
 #include <string.h>
 typedef struct info_
 {
-    char name[9];
+    char name[10];
     int height;
 } info;
-int cmp(const void *a,const void *b)
+
+int cmp(const void *a, const void *b)
 {
     if(((info *)a)->height == ((info *)b)->height)
     {
@@ -14,35 +15,36 @@ int cmp(const void *a,const void *b)
     }
     return ((info *)b)->height - ((info *)a)->height;
 }
+
 int main()
 {
-    info *group;
-    int i,j,n,k,cols,total,id,offset,*result;
-    scanf("%d%d",&n,&k);
-    group=(info *)malloc(n*sizeof(info));
+    info *input;
+    int i,j,n,k,cols,total,id,start,*result;
+    scanf("%d %d",&n,&k);
+    input = (info *)malloc(n*sizeof(info));
     result = (int *)malloc(n*sizeof(int));
     for(i=0;i<n;i++)
     {
-        scanf("%s %d",group[i].name,&(group[i].height));
+        scanf("%s %d",input[i].name,&(input[i].height));
     }
-    qsort(group,n,sizeof(info),cmp);
-    total=n;
-    offset=0;
-    id=0;
+    qsort(input,n,sizeof(info),cmp);
+    total = n;
+    id = 0;
+    start = 0;
     for(i=0;i<k;i++)
     {
-        cols=total-n/k*(k-i-1);
-        total-=cols;
+        cols = total - n/k*(k-i-1);
+        total -= cols;
         for(j=1;j<=cols;j++)
         {
-            result[offset+cols/2+(j%2==0?-j/2:j/2)]=id++;
+            result[start+cols/2+(j%2==0?-j/2:j/2)]=id++;
         }
-        printf("%s",group[result[offset]].name);
+        printf("%s",input[result[start]].name);
         for(j=1;j<cols;j++)
         {
-            printf(" %s",group[result[offset+j]].name);
+            printf(" %s",input[result[start+j]].name);
         }
         printf("\n");
-        offset+=cols;
+        start+=cols;
     }
 }
